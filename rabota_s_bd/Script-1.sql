a7f4a587b01773354e97fe471cfb4e9cad104ee7
mariaedel <edel.masha98@gmail.ru>
2021-06-20 18:31:45 +0300

    new

--- rabota_s_bd/Script-1.sql
+++ rabota_s_bd/Script-1.sql
@@ -11,3 +11,8 @@ select tc.table_name, kcu.column_name
 from information_schema.key_column_usage kcu
 join information_schema.table_constraints tc on tc.constraint_name = kcu.constraint_name 
 where tc.constraint_type = 'PRIMARY KEY'
+
+-- вывести всех неактивных покупателей (доработка)
+select customer_id
+from customer 
+where active = 1
